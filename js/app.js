a6b1e70c284feac7b7d7794479222de8d99ef481
tsneemahmad <tsneemamarat@gmail.com>
2020-03-02 14:25:55 +0200

    edit

---
+++ js/app.js
@@ -0,0 +1,123 @@
+'use strict'
+
+var product = [
+    'bag',
+    'banana',
+    'bathroom',
+    'boots',
+    'breakfast',
+    'bubblegum',
+    'chair',
+    'cthulhu',
+    'dog-duck',
+    'dragon',
+    'pen',
+    'pet-sweep',
+    'scissors',
+    'shark',
+    'sweep',
+    'tauntaun',
+    'unicorn',
+    'usb',
+    'water-can',
+    'wine-glass'
+];
+
+var firstImage = document.getElementById('firstImage');
+var secondImage = document.getElementById('secondImage');
+var rightImage = document.getElementById('thirdImage');
+var images = document.getElementById('images');
+
+firstImage.src = `img/${product[0]}.jpg`;
+firstImage.alt = product[0];
+firstImage.title = product[0];
+
+secondImage.src = `img/${product[1]}.jpg`;
+secondImage.alt = product[1];
+secondImage.title = product[1];
+
+thirdImage.src = `img/${product[2]}.jpg`;
+thirdImage.alt = product[2];
+thirdImage.title = product[2];
+
+function Products(name) {
+    this.name = name;
+    this.imagePath = `img/${this.name}.jpg`;
+    this.click = 0;
+    this.view = 0;
+    Products.all.push(this);
+}
+Products.all = [];
+
+for (var i = 0; i < product.length; i++) {
+    new Products(product[i]);
+}
+var firstProduct, secondProduct, thirdProduct;
+function render() {
+    firstProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+    console.log(firstProduct);
+    secondProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+    console.log(secondProduct);
+    thirdProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+    console.log(thirdProduct);
+    while (firstProduct.imagePath === secondProduct.imagePath || thirdProduct.imagePath === secondProduct.imagePath || thirdProduct.imagePath === firstProduct.imagePath){
+      firstProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+      secondProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+      thirdProduct = Products.all[randomNumber(0, Products.all.length - 1)];
+    }
+    firstImage.src = firstProduct.imagePath;
+    firstImage.alt = firstProduct.name;
+    firstImage.title = firstProduct.name;
+
+    secondImage.src = secondProduct.imagePath;
+    secondImage.alt = secondProduct.name;
+    secondImage.title = secondProduct.name;
+
+    thirdImage.src = thirdProduct.imagePath;
+    thirdImage.alt = thirdProduct.name;
+    thirdImage.title = thirdProduct.name;
+
+  
+}
+
+render();
+var x = 25;
+images.addEventListener('click', handleClickOnProduct);
+var totalClicks =0;
+function handleClickOnProduct(event) {
+  if(totalClicks <x) {
+    if(event.target.id !== 'images') {
+      if(event.target.id === 'firstImage') {
+        firstProduct.click++;
+      } else if(event.target.id === 'secondImage') {
+        secondProduct.click++;
+      } else if(event.target.id === 'thirdImage') {
+        thirdProduct.click++;
+      }
+      totalClicks++;
+      firstProduct.view++;
+      secondProduct.view++;
+      thirdProduct.view++;
+      render();
+    }
+  }  else {
+    console.log('more than 25 clicks');
+    images.removeEventListener('click',handleClickOnProduct);
+    render2();
+  }
+}
+function render2() {
+    var ulE1 = document.getElementById('result');
+    for (var i =0; i<Products.all.length ; i++) {
+      var liE1 = document.createElement('li');
+    //   Banana Slicer had 3 votes and was shown 5 times
+      liE1.textContent = `${Products.all[i].name} had ${Products.all[i].click} votes and was shown ${Products.all[i].view} times`;
+      ulE1.appendChild(liE1);
+      var liE1 = document.createElement('li');
+    }
+  }
+
+
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
