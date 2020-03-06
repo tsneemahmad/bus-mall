a9e3e353022d733228c9e8c09711d4e877f08edb
tsneemahmad <tsneemamarat@gmail.com>
2020-03-06 22:01:16 +0200

    new edit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bus-mall
+
+this website provides a collection of products to the user and let him/her vote to choose their favourite product, and then show them the results in a chart and a list.
--- js/app.js
+++ js/app.js
@@ -1,131 +1,8 @@
-// 'use strict'
-
-// var product = [
-//     'bag',
-//     'banana',
-//     'bathroom',
-//     'boots',
-//     'breakfast',
-//     'bubblegum',
-//     'chair',
-//     'cthulhu',
-//     'dog-duck',
-//     'dragon',
-//     'pen',
-//     'pet-sweep',
-//     'scissors',
-//     'shark',
-//     'sweep',
-//     'tauntaun',
-//     'unicorn',
-//     'usb',
-//     'water-can',
-//     'wine-glass'
-// ];
-
-// var firstImage = document.getElementById('firstImage');
-// var secondImage = document.getElementById('secondImage');
-// var rightImage = document.getElementById('thirdImage');
-// var images = document.getElementById('images');
-
-// firstImage.src = `img/${product[0]}.jpg`;
-// firstImage.alt = product[0];
-// firstImage.title = product[0];
-
-// secondImage.src = `img/${product[1]}.jpg`;
-// secondImage.alt = product[1];
-// secondImage.title = product[1];
-
-// thirdImage.src = `img/${product[2]}.jpg`;
-// thirdImage.alt = product[2];
-// thirdImage.title = product[2];
-
-// function Products(name) {
-//     this.name = name;
-//     this.imagePath = `img/${this.name}.jpg`;
-//     this.click = 0;
-//     this.view = 0;
-//     Products.all.push(this);
-// }
-// Products.all = [];
-
-// for (var i = 0; i < product.length; i++) {
-//     new Products(product[i]);
-// }
-// var firstProduct, secondProduct, thirdProduct;
-// function render() {
-//     firstProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//     console.log(firstProduct);
-//     secondProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//     console.log(secondProduct);
-//     thirdProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//     console.log(thirdProduct);
-//     while (firstProduct.imagePath === secondProduct.imagePath || thirdProduct.imagePath === secondProduct.imagePath || thirdProduct.imagePath === firstProduct.imagePath){
-//       firstProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//       secondProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//       thirdProduct = Products.all[randomNumber(0, Products.all.length - 1)];
-//     }
-//     firstImage.src = firstProduct.imagePath;
-//     firstImage.alt = firstProduct.name;
-//     firstImage.title = firstProduct.name;
-
-//     secondImage.src = secondProduct.imagePath;
-//     secondImage.alt = secondProduct.name;
-//     secondImage.title = secondProduct.name;
-
-//     thirdImage.src = thirdProduct.imagePath;
-//     thirdImage.alt = thirdProduct.name;
-//     thirdImage.title = thirdProduct.name;
-
-  
-// }
-
-// render();
-// var x = 25;
-// images.addEventListener('click', handleClickOnProduct);
-// var totalClicks =0;
-// function handleClickOnProduct(event) {
-//   if(totalClicks <x) {
-//     if(event.target.id !== 'images') {
-//       if(event.target.id === 'firstImage') {
-//         firstProduct.click++;
-//       } else if(event.target.id === 'secondImage') {
-//         secondProduct.click++;
-//       } else if(event.target.id === 'thirdImage') {
-//         thirdProduct.click++;
-//       }
-//       totalClicks++;
-//       firstProduct.view++;
-//       secondProduct.view++;
-//       thirdProduct.view++;
-//       render();
-//     }
-//   }  else {
-//     console.log('more than 25 clicks');
-//     images.removeEventListener('click',handleClickOnProduct);
-//     render2();
-//   }
-// }
-// function render2() {
-//     var ulE1 = document.getElementById('result');
-//     for (var i =0; i<Products.all.length ; i++) {
-//       var liE1 = document.createElement('li');
-//     //   Banana Slicer had 3 votes and was shown 5 times
-//       liE1.textContent = `${Products.all[i].name} had ${Products.all[i].click} votes and was shown ${Products.all[i].view} times`;
-//       ulE1.appendChild(liE1);
-//       var liE1 = document.createElement('li');
-//     }
-//   }
-
-
-//   function randomNumber(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   }
-
-
 'use strict'
+
 var allImages = [];
 
+//////////create an array for the images/////////////
 var product = [
   'bag',
   'banana',
@@ -149,11 +26,15 @@ var product = [
   'wine-glass'
 ];
 
+
+///////////////create variables for the three images////////////////
 var firstImage = document.getElementById('firstImage');
 var secondImage = document.getElementById('secondImage');
-var rightImage = document.getElementById('thirdImage');
+var thirdImage = document.getElementById('thirdImage');
 var images = document.getElementById('images');
 
+
+//////////////
 firstImage.src = `img/${product[0]}.jpg`;
 firstImage.alt = product[0];
 firstImage.title = product[0];
@@ -166,6 +47,8 @@ thirdImage.src = `img/${product[2]}.jpg`;
 thirdImage.alt = product[2];
 thirdImage.title = product[2];
 
+
+//////////////create a constructor for the products/////////////////
 function Products(name) {
   this.name = name;
   this.imagePath = `img/${this.name}.jpg`;
@@ -175,11 +58,15 @@ function Products(name) {
 }
 Products.all = [];
 
+
+///////////////create a function to change the data from obj to string the new products/////////////////
 function newProducts(){
   var productArr = JSON.stringify(Products.all);
   localStorage.setItem('productsView', productArr);
 }
 
+
+///////////////create a function to get the items from the local storage/////////////
 function getproducts() {
   var productArr = localStorage.getItem('productsView');
   console.log(productArr);
@@ -198,6 +85,9 @@ for (var i = 0; i < product.length; i++) {
   new Products(product[i]);
 }
 var firstProduct, secondProduct, thirdProduct;
+
+
+////////////////create a function to show the three products////////////////
 function render() {
   firstProduct = Products.all[randomNumber(0, Products.all.length - 1)];
   console.log(firstProduct);
@@ -230,9 +120,14 @@ function render() {
 }
 
 render();
+
 var x = 25;
 images.addEventListener('click', handleClickOnProduct);
+
 var totalClicks = 0;
+
+
+//////////////create a function for the voting////////////////
 function handleClickOnProduct(event) {
   if (totalClicks < x) {
     if (event.target.id !== 'images') {
@@ -253,20 +148,12 @@ function handleClickOnProduct(event) {
     console.log('more than 25 clicks');
     images.removeEventListener('click', handleClickOnProduct);
     chart();
-    // render2();
+    
   }
 }
-// function render2() {
-//     var ulE1 = document.getElementById('result');
-//     for (var i =0; i<Products.all.length ; i++) {
-//       var liE1 = document.createElement('li');
-//       liE1.textContent = `${Products.all[i].name} had ${Products.all[i].click} votes and was shown ${Products.all[i].view} times`;
-//       ulE1.appendChild(liE1);
-//       var liE1 = document.createElement('li');
-//     }
-//   }
 
 
+///////////////create a chart to show the results////////////////
 function chart() {
   var allProducts = [];
   var numClicks = [];
@@ -279,54 +166,12 @@ function chart() {
     var newViews = Products.all[i].view;
     numViews.push(newViews);
   }
-
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: allProducts,
-      datasets: [{
-        label: '# of Clicks',
-        data: numClicks,
-        backgroundColor: 
-          'rgba(255, 99, 132, 0.2)',
-        
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-        ],
-        borderWidth: 1
-
-      },
-      {
-        label: '# of Views',
-        data: numViews,
-        backgroundColor: 
-          'rgba(255, 99, 200 0.2)',
-        
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-        ],
-        borderWidth: 1,
-        type: 'bar',
-        labels: allProducts
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
 }
-    // chart();
+  
     newProducts();
     render2();
   
-
+////////////create a function to show the results in a list//////////////////
 function render2() {
     var ulE1 = document.getElementById('result');
     for (var i =0; i<Products.all.length ; i++) {
@@ -338,6 +183,7 @@ function render2() {
     var allProducts = [];
     var numClicks = [];
     var numViews = [];
+
     for (var i = 0; i < Products.all.length; i++) {
       var productsName = Products.all[i].name;
       allProducts.push(productsName);
@@ -389,10 +235,6 @@ function render2() {
       }
     });
   }
-
-
-// function chart() {
-// }
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
